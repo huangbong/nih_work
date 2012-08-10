0b57d26050ebc6426393e86da23a4683b1e969d4
Mitchell Huang <mhuang2286@gmail.com>
2012-08-10 14:15:43 -0400

    added final folder

---
+++ final/errorcheck2.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# Errorcheck two columm NUS tables
+# for missing points and automatically generate new ones.
+
+import sys, random
+
+def list_to_int(list):
+        new_list = []
+        for str in list:
+                new_list.append(int(str))
+        return new_list
+
+def random_gen(max):
+        v1 = random.randint(0,max)
+        v2 = random.randint(0,max)
+        while v1 == v2:
+                v1 = random.randint(0,max)
+                v2 = random.randint(0,max)
+        v = [v1, v2]
+        return v
+
+def read_file(file_name):
+    global x_row, y_row, original_lines
+
+    f = open(file_name, 'r')
+    x_row = []
+    y_row = []
+
+    for row in f:
+            row = row.split()
+            x_row.append(row[0])
+            y_row.append(row[1])
+
+    f.close()
+
+    x_row = list_to_int(x_row)
+    y_row = list_to_int(y_row)
+
+    original_lines = len(x_row)
+
+def check_x(max1, max2):
+    global are_errors
+    for x in range(max1):
+        if x not in x_row:
+            print 'Warning: x empty @ %d.' % x
+            are_errors = True
+            y = random_gen(max2)
+            x_row.append(x); y_row.append(y[0]); x_row.append(x); y_row.append(y[1])
+            print 'Generated two new values for empty value %d: [%d %d], [%d %d]' % (x, x, y[0], x, y[1])
+
+def check_y(max1, max2):
+    global are_errors
+    for y in range(max2):
+        if y not in y_row:
+            print 'Warning: y empty @ %d.' % y
+            are_errors = True
+            x = random_gen(max1)
+            y_row.append(y); x_row.append(x[0]); y_row.append(y); x_row.append(x[1])
+            print 'Generated two new values for empty value %d: [%d %d], [%d %d]' % (y, x[0], y, x[1], y)
+
+
+def error_check(file_name, max1, max2):
+    global are_errors
+
+    print 'Beginning error checking...'
+    read_file(file_name)
+
+    are_errors = False
+    print 'Checking x values...'
+    check_x(max1, max2)
+    print 'Checking y values...'
+    check_y(max1, max2)
+
+    if are_errors == True:
+        prompt = raw_input('Insert new data into file %s? (y/n) ' % file_name)
+        if prompt.lower() == 'y':
+            print 'Writing...' 
+            f = open(file_name, 'w')
+            for num in range(len(x_row)):
+               f.write('%s %s\n' % (x_row[num], y_row[num]))
+            f.close()
+            print 'File written.'
+            print '%s new lines were added.' % (len(x_row) - int(original_lines))
+        else:
+            sys.exit('Goodbye!')
+    else:
+        print 'No errors found!'
+
+    return
+
+if len(sys.argv) != 4:
+    sys.exit('Usage: errorcheck2.py [file name] [max1] [max2]')
+
+file_name = sys.argv[1]
+max1 = int(sys.argv[2])
+max2 = int(sys.argv[3])
+
+if __name__ == '__main__':
+    error_check(file_name, max1, max2)
---
+++ final/pseudoy.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# Generate pseduo y table
+
+import sys, random
+
+def list_to_int(list):
+        new_list = []
+        for str in list:
+                new_list.append(int(str))
+        list = new_list
+        return list
+
+def random_gen(max):
+        v1 = random.randint(0,max)
+        v2 = random.randint(0,max)
+        while v1 == v2:
+                v1 = random.randint(0,max)
+                v2 = random.randint(0,max)
+        v = [v1, v2]
+        return v
+
+def create_rows():
+    global x_row, y_row, z_row
+    n = open(file_name, 'r')
+    x_row = []; y_row = []; z_row = []
+    for row in n:
+            row = row.split()
+            x_row.append(row[0])
+            y_row.append(row[1])
+            z_row.append(row[2])
+    n.close()
+    x_row = list_to_int(x_row); y_row = list_to_int(y_row); z_row = list_to_int(z_row)
+
+def create_pseudoy():
+    global pseudoy_row
+    pseudoy_row = []
+    for num in range(0, len(x_row)):
+        i = y_row[num]
+        j = z_row[num]
+        pseudoy_row.append(j*max2+i)
+
+def check():
+    for x in range(max2):
+        if x not in x_row:
+            print 'X error at %s.' % x
+
+    for pseudoy in range(max3):
+        if pseudoy not in pseudoy_row:
+            print 'Pseudoy error at %s.' % pseudoy
+
+def fix_pseudoy():
+    global new_lines
+    new_lines = 0
+    for i in range(max2):
+        for j in range(max3):
+            num = j * max2 + i
+            if num not in pseudoy_row:
+                new_lines = new_lines + 2
+                randlist = random_gen(max3)
+                print 'Inserting [%s, %s, %s]' % (randlist[0], i, j)
+                print 'Inserting [%s, %s, %s]' % (randlist[1], i, j)
+                n = open(file_name, 'a')
+                n.write('%s %s %s\n' % (randlist[0], i, j))
+                n.write('%s %s %s\n' % (randlist[1], i, j))
+                n.close
+
+def main():
+    create_rows()
+    create_pseudoy()
+    check()
+    print 'Fixing pseudoy...'
+    fix_pseudoy()
+    print 'Done.'
+    print '%s new lines were added.' % new_lines
+
+if len(sys.argv) < 4:
+        sys.exit('Usage: pseudoy.py [input file] [max2] [max3]')
+
+file_name = sys.argv[1]
+max2 = int(sys.argv[2])
+max3 = int(sys.argv[3])
+
+if __name__ == "__main__":
+    main()
